0e10b058ebe50953e5ece63bac8c79b23a8145f3
Jonathan Pryor <jonpryor@vt.edu>
2025-07-02 12:52:13 -0400

    Add documentation
    Shorten and embiggen the app name at the top of the screen, and add a
    `info.circle` ⓘ button to the right.  Tapping the ⓘ button will
    show brief usage docs in a sheet, which can be dismissed by tapping
    the Dismiss button.
    
    Update the **Matches** section to print "No matching words found" if
    there are no matches, and if all textboxes are empty, *also* show
    "Try entering some letters?"

--- FiveLetterWords/CharacterInputView.swift
+++ FiveLetterWords/CharacterInputView.swift
@@ -7,9 +7,22 @@
 
 import SwiftUI
 
+let appDocsMarkdown = """
+    Do you ever have problems thinking of five letter words with letters in a given position?
+
+    Tap within one of the text boxes underneath **Letters**, enter a letter, then tap **Search**.
+
+    All words with letters in the given position are shown underneath **Matches**.
+    """
+
+let appDocs = try! AttributedString(
+    markdown: appDocsMarkdown,
+    options: AttributedString.MarkdownParsingOptions(interpretedSyntax: .inlineOnlyPreservingWhitespace))
+
 struct CharacterInputView: View {
     @State private var xmatches : [String] = []
     @State private var xtemplate : [String] = ["", "", "", "", ""]
+    @State private var documentationPopover : DocumentationPopoverModel?
     @FocusState private var showKeyboard : Bool
 
     init() {
@@ -18,7 +31,26 @@ struct CharacterInputView: View {
 
     var body: some View {
         VStack(alignment:.leading) {
-            Text("Find five letter words!")
+            HStack {
+                Text("Find words!")
+                    .font(.largeTitle)
+                Button {
+                    documentationPopover = DocumentationPopoverModel(docs: appDocs)
+                } label: {
+                    Image(systemName: "info.circle")
+                        .imageScale(.large)
+                }
+                .popover(item: $documentationPopover) { detail in
+                    Text("Find Five Letter Words")
+                        .font(.title)
+                    Text("\(detail.docs)")
+                        .padding()
+                    Button("Dismiss") {
+                        documentationPopover = nil
+                    }
+                }
+            }
+            Spacer()
             Text("Letters")
                 .font(.title)
             HStack {
@@ -45,6 +77,14 @@ struct CharacterInputView: View {
             Spacer()
             Text("Matches")
                 .font(.title)
+            if (xmatches.count == 0) {
+                Text("No matching words found.")
+                    .italic()
+                if xtemplate.allSatisfy({ $0.isEmpty }) {
+                    Text("Try entering some letters?")
+                        .italic()
+                }
+            }
             List {
                 ForEach(0..<xmatches.count, id: \.description) { index in
                     Text(xmatches[index])
@@ -69,6 +109,9 @@ struct CharacterInputView: View {
         print("# jonp: search…? xtemplate=\(xtemplate)")
         showKeyboard = false
         xmatches = matches(template: xtemplate)
+    }
+
+    private func didDismissDocs() {
     }
 }
 
@@ -100,6 +143,11 @@ extension View {
     func limitInputLength(value: Binding<String>, length: Int) -> some View {
         self.modifier(TextFieldLimitModifier(value: value, length: length))
     }
+}
+
+struct DocumentationPopoverModel : Identifiable {
+    var id : String { docs.description }
+    let docs : AttributedString
 }
 
 #Preview {
